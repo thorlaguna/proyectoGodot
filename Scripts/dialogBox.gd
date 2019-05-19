4dd236987a44223261031d5f5f9b94a8ed19a081
Victor Laguna <laguna.bavic18@zaragoza.salesianos.edu>
2019-05-19 19:59:01 +0200

    commit 1
    Signed-off-by: Victor Laguna <laguna.bavic18@zaragoza.salesianos.edu>

---
+++ Scripts/dialogBox.gd
@@ -0,0 +1,13 @@
+#dialogBox.gd
+extends RichTextLabel
+
+var dialog = ["Kio peire comeme los cojones y toma el puto dialog box tolay. Lo implementas como te salga de los cojones."]
+var page = 0
+
+func _ready():
+	set_bbcode(dialog[page])
+	set_visible_characters(0)
+
+
+func _on_Timer_timeout():
+	set_visible_characters(get_visible_characters()+1)
